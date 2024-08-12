98a70bc816699538ca1d453dd9f66445dcceb0a0
rajahwu <vmradford@gmail.com>
2024-08-12 05:29:38 -0400

    build: config react router to work with webpack

--- prod/src/App.js
+++ prod/src/App.js
@@ -1,16 +1,39 @@
 // src/App.js
 import React from 'react';
 import {
+  Link,
+  Outlet,
   Route,
   Routes
 } from "react-router-dom";
 import SignIn from './components/auth/sign-in';
 
+const RootLayout = () => {
+  return (
+    <div>
+      <h1>Root Layout</h1>
+      <Outlet />
+    </div>
+  )
+}
+
 const App = () => {
   return (
+    <>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="dashboard">Dashboard</Link>
+      <Link to="profile">Profile</Link>
+      <Link to="/">Sign In</Link>
+    </div>
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<RootLayout />} errorElement={<div>Nope</div>}>
+      <Route index element={<SignIn />} />
+      <Route path="dashboard" element={<div>Dashboard</div>} />
+      <Route path="profile" element={<div>Profile</div>} />
+      </Route>
     </Routes>
+    </>
   )
 }
 
--- prod/webpack.config.js
+++ prod/webpack.config.js
@@ -42,5 +42,6 @@ module.exports = {
   },
   devServer: {
     port: 3000,
+    historyApiFallback: true,
   },
 };
